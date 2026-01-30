601e8c0e17a9f9512496200d17ea15f022540f17
BmtUltra <1453935889@qq.com>
2026-01-30 22:03:34 +0800

    修正模型

--- src/main/java/com/bmt/kaleidoscope_end/registry/KEFoodBiteRegistry.java
+++ src/main/java/com/bmt/kaleidoscope_end/registry/KEFoodBiteRegistry.java
@@ -3,6 +3,7 @@ package com.bmt.kaleidoscope_end.registry;
 import com.bmt.kaleidoscope_end.KaleidoscopeEnd;
 import com.github.ysbbbbbb.kaleidoscopecookery.init.registry.FoodBiteRegistry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 
 public class KEFoodBiteRegistry {
     public static ResourceLocation DARK_DRAGON_EGG_STEW;
@@ -12,6 +13,7 @@ public class KEFoodBiteRegistry {
         DARK_DRAGON_EGG_STEW = registry.registerFoodData(KaleidoscopeEnd.id("dark_dragon_egg_stew"), FoodBiteRegistry.FoodData
                 .create(4, KEFoods.DARK_DRAGON_EGG_STEW_BLOCK, KEFoods.DARK_DRAGON_EGG_STEW_ITEM)
                 .setLootItem(() -> KEItem.DRAGON_EGG_SHELL.get())
+                .setAABB(Block.box(0, 0, 0, 16, 11, 16))
         );
 
     }
