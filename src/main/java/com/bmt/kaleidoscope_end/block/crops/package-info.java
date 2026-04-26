f053cee4f2fcdeef245856b9332f9711f7b3e1bf
BmtUltra <1453935889@qq.com>
2026-04-26 18:06:30 +0800

    更新

---
+++ src/main/java/com/bmt/kaleidoscope_end/block/crops/package-info.java
@@ -0,0 +1,9 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@FieldsAreNonnullByDefault
+package com.bmt.kaleidoscope_end.block.crops;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
